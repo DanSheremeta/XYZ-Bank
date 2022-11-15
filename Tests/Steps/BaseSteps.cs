8752955a327a31adb6e6b279b1ba76c89114e51e
DanSheremeta <danylo3002@gmail.com>
2022-11-15 14:21:48 +0200

    Добавьте файлы проекта.

---
+++ Tests/Steps/BaseSteps.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace Tests.Steps
+{
+    [Binding]
+    public class BaseSteps
+    {
+        protected static IWebDriver driver;
+
+        [BeforeFeature]
+        public static void BeforeFeauture()
+        {
+            driver = new EdgeDriver();
+            driver.Navigate().GoToUrl("https://www.globalsqa.com/angularJs-protractor/BankingProject/");
+        }
+
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            driver.Close();
+        }
+    }
+}
